f4e53a543f9195e72c66321adcd9a4ea6a142f17
hassanxadel <89996749+hassanxadel@users.noreply.github.com>
2024-05-05 04:20:14 +0300

    activitydetails.php

--- activitydetails.php
+++ activitydetails.php
@@ -1,76 +1,111 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity Details CRUD</title>
+</head>
+<body>
+    <h2>Activity Details CRUD</h2>
 
-class ActivityDetailsCRUD {
-    private $conn;
+    <h3>Create Activity</h3>
+    <form action="ActivityDetailsController.php" method="post">
+        Activity ID: <input type="text" name="activity_id"><br>
+        Activity Name: <input type="text" name="Activity_Name"><br>
+        Instructor ID: <input type="text" name="user_id"><br>
+        Attachment: <input type="text" name="attachment"><br>
+        Start Date: <input type="date" name="start_date"><br>
+        End Date: <input type="date" name="end_date"><br>
+        Activity Field: <input type="text" name="activity_field"><br>
+        <input type="submit" value="Create">
+    </form>
 
-    public function __construct() {
-        // Database connection parameters
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "agileproject";
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Instructor Name</th>
+    <th>Teaching Field</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Dr. Mahmoud Samir</td>
+    <td>Data Analysis</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Dr. Tamer Salama</td>
+    <td>Mobile Programming</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Dr. Ahmed Khaled</td>
+    <td>Web Development</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>Dr. Mohamed Hassan</td>
+    <td>Digital Marketing</td>
+  </tr>
+</table>
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Activity Fileds</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Data Analysis</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Mobile Programming</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Web Development</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>Digital Marketing</td>
+  </tr>
+</table>
 
-        // Create connection
-        $this->conn = new mysqli($servername, $username, $password, $database);
 
-        // Check connection
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
+    <h3>Read All Activity Details</h3>
+
+    <?php
+    require_once 'ActivityDetailsCRUD.php';
+
+    $activityDetailsCRUD = new ActivityDetailsCRUD();
+
+    $activityDetails = $activityDetailsCRUD->readAll();
+    if (!empty($activityDetails)) {
+        echo "<ul>";
+        foreach ($activityDetails as $detail) {
+            echo "<li> ". ", Activity ID: " . $detail['activity_id'] ."<br>". ", Instructor ID: " . $detail['user_id']."<br>" . ", Start Date: " . $detail['start_date']."<br>" . ", End Date: " . $detail['end_date'] ."<br>". ", Activity Name: " . $detail['Activity_Name'] ."<br>". ", Attachment: " . $detail['attachment']."<br>".",Activity Field:" . $detail['activity_field'] ."</li>";
         }
+        echo "</ul>";
+    } else {
+        echo "No activity details found";
     }
+    ?>
 
-    // Create
-    public function create($activity_id, $user_id, $Instructor, $start_date, $End_date, $Activity_Name, $attachment) {
-        $sql = "INSERT INTO activity_details (activity_id, user_id, Instructor, start_date, end_date, Activity_Name, attachment)
-                VALUES ('$activity_id', '$user_id', '$Instructor', '$start_date', '$End_date', '$Activity_Name', '$attachment')";
+    <h3>Update Activity Detail</h3>
+    <form action="ActivityDetailsController.php" method="post">
+        Activity ID: <input type="text" name="activity_id"><br>
+        Activity Name: <input type="text" name="Activity_Name"><br>
+        Instructor ID: <input type="text" name="user_id"><br>
+        Attachment: <input type="text" name="attachment"><br>
+        Start Date: <input type="date" name="start_date"><br>
+        End Date: <input type="date" name="end_date"><br>
+        Activity Field: <input type="text" name="activity_field"><br>
+        <input type="submit" value="Update">
+    </form>
 
-        if ($this->conn->query($sql) === TRUE) {
-            return "New record created successfully";
-        } else {
-            return "Error: " . $sql . "<br>" . $this->conn->error;
-        }
-    }
-
-    // Read
-    public function readAll() {
-        $sql = "SELECT * FROM activity_details";
-        $result = $this->conn->query($sql);
-        $activityDetails = [];
-
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                $activityDetails[] = $row;
-            }
-        }
-
-        return $activityDetails;
-    }
-
-    // Update
-    public function update($id, $activity_id, $user_id, $Instructor, $start_date, $End_date, $Activity_Name, $attachment) {
-        $sql = "UPDATE activity_details SET activity_id='$activity_id', user_id='$user_id', Instructor='$Instructor', start_date='$start_date', end_date='$End_date', Activity_Name='$Activity_Name', attachment='$attachment' WHERE id=$id";
-
-        if ($this->conn->query($sql) === TRUE) {
-            return "Record updated successfully";
-        } else {
-            return "Error updating record: " . $this->conn->error;
-        }
-    }
-
-    // Delete
-    public function delete($id) {
-        $sql = "DELETE FROM activity_details WHERE id=$id";
-
-        if ($this->conn->query($sql) === TRUE) {
-            return "Record deleted successfully";
-        } else {
-            return "Error deleting record: " . $this->conn->error;
-        }
-    }
-
-    public function __destruct() {
-        $this->conn->close();
-    }
-}
-
-
+    <h3>Delete Activity Detail</h3>
+    <form action="ActivityDetailsController.php" method="post">
+        ID: <input type="text" name="id"><br>
+        <input type="submit" value="Delete">
+    </form>
+</body>
+</html>
